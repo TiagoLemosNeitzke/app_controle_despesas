c63cf00ad6c6deedcb5375321b830955513ca757
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2024-01-18 15:10:36 -0400

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(const ExpenseApp());
+
+class ExpenseApp extends StatelessWidget {
+  const ExpenseApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Expense App')),
+      body: const Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          SizedBox(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                elevation: 5,
+                child: Text('Gráfico'),
+              )),
+          Card(
+            child: Text('Lista de Transações'),
+          ),
+        ],
+      ),
+    );
+  }
+}
