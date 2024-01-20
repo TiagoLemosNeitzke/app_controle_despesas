98bdc3c386b9c00de92567234588018370b80ed7
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2024-01-20 08:36:57 -0400

    criando fomrulário

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,147 @@
 import 'package:flutter/material.dart';
+import 'models/transaction.dart';
+import 'package:intl/intl.dart';
 
-main() => runApp(const ExpenseApp());
+main() => runApp(ExpenseApp());
 
 class ExpenseApp extends StatelessWidget {
-  const ExpenseApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    )
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Expense App')),
-      body: const Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+      appBar: AppBar(
+          title: const Text('Despesas Pessoais'),
+          centerTitle: true,
+          backgroundColor: Colors.purple,
+          titleTextStyle: const TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          )),
+      body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          SizedBox(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                elevation: 5,
-                child: Text('Gráfico'),
-              )),
-          Card(
-            child: Text('Lista de Transações'),
+          Container(
+              child: const Card(
+            color: Colors.blue,
+            child: Text('Gráfico'),
+            elevation: 5,
+          )),
+          Column(
+            children: _transactions.map((transacao) {
+              return Card(
+                child: Row(children: <Widget>[
+                  Container(
+                    width: 150,
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 10,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
+                    ),
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      'R\$ ${transacao.value.toStringAsFixed(2)}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        transacao.title,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        DateFormat('dd/MM/yyyy').format(transacao.date),
+                        style: TextStyle(
+                          color: Colors.grey[700],
+                        ),
+                      ),
+                    ],
+                  )
+                ]),
+              );
+            }).toList(),
           ),
+          Card(
+            elevation: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    controller: titleController,
+                    decoration: const InputDecoration(
+                      labelText: 'Título',
+                    ),
+                  ),
+                  TextField(
+                    controller: valueController,
+                    decoration: const InputDecoration(
+                      labelText: 'Valor (R\$)',
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          print(titleController.text);
+                          print(valueController.text);
+                        },
+                        child: const Text(
+                          'Nova Transação',
+                          style: TextStyle(
+                            color: Colors.purple,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
